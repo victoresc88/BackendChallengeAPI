1b4d2e78c018e0da067ecabf6418140bf4e6c79a
vicesc88 <vicesc88@gmail.com>
2021-01-30 17:47:53 +0100

    Add product model

---
+++ Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendChallengeAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Supplier { get; set; }
+        public string Name { get; set; }
+        public decimal Rate { get; set; }
+        public decimal DailyStandingCharge { get; set; }
+        public int ContractLength { get; set; }
+        public int Renewable { get; set; }
+        public string Status { get; set; }
+    }
+}
