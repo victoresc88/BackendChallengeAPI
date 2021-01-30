e2fa94f4f3ffe9395c01fc5f67334cb474a89721
vicesc88 <vicesc88@gmail.com>
2021-01-30 17:38:17 +0100

    Clean default example template

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BackendChallengeAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        public ProductController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IEnumerable<int> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
